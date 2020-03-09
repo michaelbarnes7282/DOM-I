8520975ef93375fd1f4e4c597d7ff1f18f8f40a3
DESKTOP-J2DM1UE\Michael <michaelbarnes7282>
2020-03-09 14:50:06 -0700

    finished all

--- js/index.js
+++ js/index.js
@@ -40,3 +40,81 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+
+// nav bar
+let links = document.getElementsByTagName('a');
+Array.from(links).forEach ( (link, i) => {
+  link.textContent = siteContent.nav[`nav-item-${i+1}`];
+})
+
+// cta
+document.querySelector(".cta-text h1").innerHTML = siteContent.cta.h1.split(" ").join("<br> ");
+
+
+let button = document.querySelector('.cta-text button');
+button.textContent = siteContent['cta']['button'];
+
+let ctaImg = document.getElementById('cta-img');
+ctaImg.setAttribute('src', siteContent['cta']['img-src']);
+
+
+// main-content
+let featureHeader = document.querySelector('.main-content .text-content h4');
+featureHeader.textContent = siteContent['main-content']['features-h4'];
+
+let featureBody = document.querySelector('.main-content .text-content p');
+featureBody.textContent = siteContent['main-content']['features-content'];
+
+let aboutHeader = document.querySelector('.top-content .text-content:nth-of-type(2) h4');
+aboutHeader.textContent = siteContent['main-content']['about-h4'];
+
+let aboutBody = document.querySelector('.top-content .text-content:nth-of-type(2) p');
+aboutBody.textContent = siteContent['main-content']['about-content'];
+
+let midImg = document.getElementById('middle-img');
+midImg.setAttribute('src', siteContent['main-content']['middle-img-src']);
+
+let serviceHead = document.querySelector('.bottom-content .text-content h4');
+serviceHead.textContent = siteContent['main-content']['services-h4'];
+
+let serviceBody = document.querySelector('.bottom-content .text-content p');
+serviceBody.textContent = siteContent['main-content']['services-content'];
+
+let productHead = document.querySelector('.bottom-content .text-content:nth-of-type(2) h4');
+productHead.textContent = siteContent['main-content']['product-h4'];
+
+let productBody = document.querySelector('.bottom-content .text-content:nth-of-type(2) p');
+productBody.textContent = siteContent['main-content']['product-content'];
+
+let visionHead = document.querySelector('.bottom-content .text-content:nth-of-type(3) h4');
+visionHead.textContent = siteContent['main-content']['vision-h4'];
+
+let visionBody = Body = document.querySelector('.bottom-content .text-content:nth-of-type(3) p');
+visionBody.textContent = siteContent['main-content']['vision-content'];
+
+
+// contact
+let contactHead = document.querySelector('.contact h4');
+contactHead.textContent = siteContent['contact']['contact-h4'];
+
+let address = document.querySelector('.contact p');
+address.textContent = siteContent.contact['address'];
+
+let phone = document.querySelector('.contact p:nth-of-type(2)');
+phone.textContent = siteContent.contact['phone'];
+
+let email = document.querySelector('.contact p:nth-of-type(3)');
+email.textContent = siteContent.contact['email'];
+
+//footer
+let footer = document.querySelector('footer p');
+footer.textContent = siteContent.footer['copyright']; 
+
+nav = document.querySelector('header nav');
+let newA = document.createElement('a');
+let newA2 = document.createElement('a');
+newA.textContent = "Hello World!";
+newA2.textContent = "Hello World!";
+nav.append(newA);
+nav.prepend(newA2);
